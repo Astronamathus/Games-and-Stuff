d12f7701db03a67e190656a5424535d52330a7dc
Astro <82964942+Astronamathus@users.noreply.github.com>
2026-03-31 23:54:53 +0530

    Added input for number of simulations and percentage

--- headsortails.py
+++ headsortails.py
@@ -2,18 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-labels = ["Heads", "Tails"]
-output = [0,0]
+H = 0
+T = 0
 
-for i in range(10000):
+labels = ["Heads", "Tails"]
+n = int(input("Number of times to run simulation: "))
+
+for i in range(n):
     if(random.randint(0, 1) == 0):
-        output[0] += 1 
+        H += 1 
     else:
-        output[1] += 1
-    
-print(f"Tails: {output[0]}")
-print(f"Heads: {output[1]}")
+        T += 1
+
+
+output = [H,T]
+
+print(f"Heads: {output[0]}")
+print(f"Tails: {output[1]}")
+print(f"Heads%: {(output[0])/n:.2%}")
+print(f"Tails%: {(output[1])/n:.2%}")
 plt.xlabel("Heads/Tails")
 plt.ylabel("Frequency")
-plt.bar(labels, output, color='skyblue')
+plt.bar(labels, output, color='blue')
 plt.show()
